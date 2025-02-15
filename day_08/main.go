cdb5aaf5a0d61b9962f55af39ceea424da8e8d6e
Asgeir Melling <asgmel@gmail.com>
2025-02-15 14:36:38 +0100

    feat: finish day 8

--- day_08/main.go
+++ day_08/main.go
@@ -1,19 +1,65 @@
 package dayEight
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/Asgmel/advent_of_code_2024/internal/common"
 	"github.com/Asgmel/advent_of_code_2024/internal/input"
 )
 
+type bounds struct {
+	minX int
+	minY int
+	maxX int
+	maxY int
+}
+
+func newBounds(puzzleInput [][]string) bounds {
+	return bounds{
+		minX: 0,
+		maxX: len(puzzleInput[0]) - 1,
+		minY: 0,
+		maxY: len(puzzleInput) - 1,
+	}
+}
+
 func getDistanceBetweenNodes(n1, n2 common.Position) common.Position {
 	return common.Position{X: n2.X - n1.X, Y: n2.Y - n1.Y}
 }
 
-func getAntinodes(n1, n2 common.Position) (common.Position, common.Position) {
-	distance := getDistanceBetweenNodes(n1, n2)
-	return common.Position{X: n1.X - distance.X, Y: n1.Y - distance.Y}, common.Position{X: n2.X + distance.X, Y: n2.Y + distance.Y}
+func getAntinodesInBounds(p1, p2 common.Position, distance common.Position, bounds bounds) (antinodes []common.Position) {
+	potentialAntinodes := []common.Position{
+		{X: p1.X - distance.X, Y: p1.Y - distance.Y},
+		{X: p2.X + distance.X, Y: p2.Y + distance.Y},
+	}
+	for _, antinode := range potentialAntinodes {
+		if checkWithinBounds(antinode, bounds) {
+			antinodes = append(antinodes, antinode)
+		}
+	}
+	return
+
+}
+
+func getAllNodesInBounds(p1, p2 common.Position, distance common.Position, bounds bounds) (nodes []common.Position) {
+	for {
+		nodes = append(nodes, p1)
+		p1 = common.Position{X: p1.X - distance.X, Y: p1.Y - distance.Y}
+		if !checkWithinBounds(p1, bounds) {
+			break
+		}
+	}
+
+	for {
+		nodes = append(nodes, p2)
+		p2 = common.Position{X: p2.X + distance.X, Y: p2.Y + distance.Y}
+		if !checkWithinBounds(p2, bounds) {
+			break
+		}
+	}
+
+	return
 }
 
 func formatInput(puzzleInput [][]string) map[string][]common.Position {
@@ -34,40 +80,79 @@ func formatInput(puzzleInput [][]string) map[string][]common.Position {
 	return letterMap
 }
 
-func checkWithinBounds(position common.Position, width, height int) bool {
-	return position.X >= 0 && position.X < width && position.Y >= 0 && position.Y < height
+func checkWithinBounds(position common.Position, bounds bounds) bool {
+	return position.X >= bounds.minX && position.X <= bounds.maxX && position.Y >= bounds.minY && position.Y <= bounds.maxY
 }
 
-func getAntinodesFromInput(formattedInput map[string][]common.Position) (antennas []common.Position, antinodes []common.Position) {
+func getAntennas(formattedInput map[string][]common.Position) []common.Position {
+	antennas := []common.Position{}
+	for _, value := range formattedInput {
+		antennas = append(antennas, value...)
+	}
+	return antennas
+}
+
+func getAntennaPairs(formattedInput map[string][]common.Position) [][2]common.Position {
+	antennaPairs := [][2]common.Position{}
 	for _, positions := range formattedInput {
-		antennas = append(antennas, positions...)
 		for i := 0; i < len(positions)-1; i++ {
 			for j := i; j < len(positions); j++ {
 				if i == j {
 					continue
 				}
-				a1, a2 := getAntinodes(positions[i], positions[j])
-				antinodes = append(antinodes, a1, a2)
+				antennaPairs = append(antennaPairs, [2]common.Position{positions[i], positions[j]})
 			}
 		}
+	}
+	return antennaPairs
+}
+
+func filterOverlappingNodes(antinodes []common.Position, antennas []common.Position) (validAntinodes []common.Position) {
+antinodeLoop:
+	for _, antinode := range antinodes {
+		for _, antenna := range antennas {
+			if antinode == antenna {
+				continue antinodeLoop
+			}
+		}
+		validAntinodes = append(validAntinodes, antinode)
 	}
 	return
 }
 
-func countValidResults(antinodes []common.Position, antennas []common.Position, maxWidth, maxHeight int) int {
-	validAntinodes := []common.Position{}
-antinodeLoop:
-	for _, antinode := range antinodes {
-		if checkWithinBounds(antinode, maxWidth, maxHeight) {
-			for _, antenna := range antennas {
-				if antinode == antenna {
-					continue antinodeLoop
-				}
-			}
-			validAntinodes = append(validAntinodes, antinode)
-		}
+func getNodesFromAntennaPairs(
+	antennaPairs [][2]common.Position,
+	bounds bounds,
+	nodeFunc func(common.Position, common.Position, common.Position, bounds) []common.Position) (nodes []common.Position) {
+	for _, antennaPair := range antennaPairs {
+		distance := getDistanceBetweenNodes(antennaPair[0], antennaPair[1])
+		a1 := antennaPair[0]
+		a2 := antennaPair[1]
+		nodes = append(nodes, nodeFunc(a1, a2, distance, bounds)...)
 	}
-	return len(validAntinodes)
+	return
+}
+
+func printNodes(puzzleInput [][]string, nodes []common.Position) {
+	for _, node := range nodes {
+		puzzleInput[node.Y][node.X] = "#"
+	}
+
+	for _, row := range puzzleInput {
+		fmt.Println(row)
+	}
+}
+
+func getUniqueNodes(nodes []common.Position) (uniqueNodes []common.Position) {
+	nodesMap := map[common.Position]struct{}{}
+	for _, node := range nodes {
+		nodesMap[node] = struct{}{}
+	}
+
+	for key := range nodesMap {
+		uniqueNodes = append(uniqueNodes, key)
+	}
+	return
 }
 
 func Run() (func() string, func() string) {
@@ -76,13 +161,19 @@ func Run() (func() string, func() string) {
 
 func taskOne() string {
 	puzzleInput := input.ReadInputLetters(8, false)
+	bounds := newBounds(puzzleInput)
 	formattedInput := formatInput(puzzleInput)
-	antennas, antinodes := getAntinodesFromInput(formattedInput)
-	validAntinodesCount := countValidResults(antinodes, antennas, len(puzzleInput[0]), len(puzzleInput))
-	return strconv.Itoa(validAntinodesCount)
+	antennas := getAntennas(formattedInput)
+	antennaPairs := getAntennaPairs(formattedInput)
+	antinodes := getNodesFromAntennaPairs(antennaPairs, bounds, getAntinodesInBounds)
+	return strconv.Itoa(len(filterOverlappingNodes(antinodes, antennas)))
 }
 
 func taskTwo() string {
-	// return strconv.Itoa(sum)
-	return "not implemented"
+	puzzleInput := input.ReadInputLetters(8, false)
+	bounds := newBounds(puzzleInput)
+	formattedInput := formatInput(puzzleInput)
+	antennaPairs := getAntennaPairs(formattedInput)
+	antinodes := getNodesFromAntennaPairs(antennaPairs, bounds, getAllNodesInBounds)
+	return strconv.Itoa(len(getUniqueNodes(antinodes)))
 }
